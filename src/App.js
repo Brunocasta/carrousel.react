4e58c296146fd3bd804fc956a5f3d5ead2210fb9
Brunocasta <castabruno92@gmail.com>
2021-09-16 10:32:53 -0300

    cambios

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import './App.css';
-import Slider from '../public'
+import Carousel from './components/Slider';
+import React from "react";
 
 function App() {
   return (
-    <>
-<Slider/>
-    </>
+  <div className="App">
+  <Carousel/>
+  </div>
   );
 }
 
---
+++ src/components/Slider.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+
+function Carousel() {
+    return (
+       <div className="carousel">
+dfsdacsdfs cs
+       </div>
+    )
+}
+
+export default Carousel;
