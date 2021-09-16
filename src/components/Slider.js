1cd3c2f3e7bd0b5d90afb0ec984c2fdd8208acd7
Brunocasta <castabruno92@gmail.com>
2021-09-16 12:39:40 -0300

    cambios

--- src/components/Slider.js
+++ src/components/Slider.js
@@ -1,15 +1,29 @@
 import React, { useState } from 'react';
 import './carousel.css';
 import { images } from '../Data/dataSlider';
+import ArrowBackIosIcon from '@material-ui/icons/ArrowBackIos';
+import ArrowForwardIosIcon from '@material-ui/icons/ArrowForwardIos';
 
 
 function Carousel() {
-    const [currImg ,setCurrImg] = useState(2);
+    const [currImg ,setCurrImg] = useState(0);
 
     return (
         <div className="carousel">
             <div 
             className="carouselInner" style={{backgroundImage:`url(${images[currImg].img})`}}>
+                <div className="left" 
+                onClick={()=>{
+                    currImg > 0 && setCurrImg(currImg - 1);}}>
+                    <ArrowBackIosIcon/>
+                </div>
+                <div classname="center"></div>
+                <div className="right" 
+                onClick={()=>{
+                    currImg < images.length-1 && setCurrImg(currImg + 1);}}>
+                    <ArrowForwardIosIcon/>
+                </div>
+                
               
             </div>
         </div>
