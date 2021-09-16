36e0ca79ad95a075d0388a4bad93ae8f8f553860
Brunocasta <castabruno92@gmail.com>
2021-09-16 13:32:04 -0300

    cambios

--- src/components/Slider.js
+++ src/components/Slider.js
@@ -6,25 +6,31 @@ import ArrowForwardIosIcon from '@material-ui/icons/ArrowForwardIos';
 
 
 function Carousel() {
-    const [currImg ,setCurrImg] = useState(0);
+    const [currImg, setCurrImg] = useState(0);
 
     return (
         <div className="carousel">
-            <div 
-            className="carouselInner" style={{backgroundImage:`url(${images[currImg].img})`}}>
-                <div className="left" 
-                onClick={()=>{
-                    currImg > 0 && setCurrImg(currImg - 1);}}>
-                    <ArrowBackIosIcon/>
+            <div
+                className="carouselInner" style={{ backgroundImage: `url(${images[currImg].img})` }}>
+                <div className="left"
+                    onClick={() => {
+                        currImg > 0 && setCurrImg(currImg - 1);
+                    }}>
+                    <ArrowBackIosIcon />
                 </div>
-                <div classname="center"></div>
-                <div className="right" 
-                onClick={()=>{
-                    currImg < images.length-1 && setCurrImg(currImg + 1);}}>
-                    <ArrowForwardIosIcon/>
+
+                <div classname="center"><div>
+                    <h1>{images[currImg].title}</h1>
+                    <p>{images[currImg].subTitle}</p>
                 </div>
-                
-              
+
+                </div>
+                <div className="right"
+                    onClick={() => {
+                        currImg < images.length - 1 && setCurrImg(currImg + 1);
+                    }}>
+                    <ArrowForwardIosIcon />
+                </div>
             </div>
         </div>
     )
